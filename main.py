883913b38c494788ed6a9acbfdf77074f1099e86
hommat <zmateusz1111@gmail.com>
2020-02-02 15:35:57 +0100

    Make manager stores data in text file

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import sys
 import os
 
-todos = ["but milk", "buy coffe"]
+todos = []
 
 def print_menu(): 
-  os.system("clear")
   print("Todo manager\n")
   print("1. Show todos")
   print("2. Add todo")
@@ -23,30 +22,56 @@ def print_todos():
 def add_todo():
   name = input("Enter new todo name: ")
   todos.append(name)
+
+  with open("todos.txt", "a") as todos_file:
+    todos_file.write("\n{}".format(name))
+
   print("'{}' has been added to your todos".format(name))
 
-def update_todo():
+def get_validated_todo_id():
   try:
     todo_id = int(input("Enter todo id: ")) - 1
     if todo_id >= 0 and todo_id < len(todos):
-      print("Old name: {}".format(todos[todo_id]))
-      new_name = input("New name: ")
-      todos[todo_id] = new_name
-      print("Updated successfully")
+      return todo_id, True 
     else:
-      print("Entered invalid todo id")
+      return -1, False
   except:
+      return -1, False
+
+def update_todo():
+  todo_id, is_valid = get_validated_todo_id()
+  if is_valid:
+    print("Old name: {}".format(todos[todo_id]))
+    new_name = input("New name: ")
+    with open("todos.txt", "r") as todos_file:
+      lines = todos_file.readlines()
+    with open("todos.txt", "w") as todos_file:
+      for line in lines:
+        if line.strip("\n") == todos[todo_id]:
+          todos_file.write("{}\n".format(new_name))
+        else:
+          todos_file.write(line)
+
+    todos[todo_id] = new_name
+    print("Updated successfully")
+  else:
     print("Entered invalid todo id")
+ 
     
 def delete_todo():
-  try:
-    todo_id = int(input("Enter todo id: ")) - 1
-    if todo_id >= 0 and todo_id < len(todos):
-      del todos[todo_id]
-      print("Deleted successfully")
-    else:
-      print("Entered invalid todo id")
-  except:
+  todo_id, is_valid = get_validated_todo_id()
+
+  if is_valid:
+    del todos[todo_id]
+    with open("todos.txt", "r") as todos_file:
+      lines = todos_file.readlines()
+    with open("todos.txt", "w") as todos_file:
+      for (index, line) in enumerate(lines):
+        if index != todo_id:
+          todos_file.write(line)
+
+    print("Deleted successfully")
+  else:
     print("Entered invalid todo id")
 
 def close():
@@ -71,6 +96,18 @@ def select_option():
 
   input("\nPress any key to continue...")
 
+def load_todos():
+  try:
+    with open("todos.txt", "r") as todos_file:
+      for todo in todos_file:
+        todos.append(todo.strip())
+  except:
+    todos_file = open("todos.txt", "w")
+    todos_file.close()
+
+load_todos()
 while True:
+  os.system("clear")
   print_menu()
   select_option()
+
